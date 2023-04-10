635b554e0a73a959cee6e56a3294ff4449af9a94
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-10 16:59:21 -0700

    hooked up work

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Work from "@/components/Work";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Work />
     </main>
   );
 }
