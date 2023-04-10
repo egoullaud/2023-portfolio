ae343f5a6998d3c942d0dc3d2c68dc80e40b7a2c
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-10 12:40:23 -0700

    added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
+import { Open_Sans, Lora } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin-ext"],
+  variable: "--openSans-font",
+});
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--lora-font",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${openSans.variable}, ${lora.variable}`}>
+      <NavBar />
+      <Component {...pageProps} />
+    </main>
+  );
 }
