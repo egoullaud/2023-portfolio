a491940eb534c998af1ffe68821315e9498c9d0a
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-10 16:15:28 -0700

    fixed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -17,33 +17,33 @@ export default function NavBar() {
   };
 
   return (
-    <nav>
+    <nav class>
       {/* Mobile */}
-      <div className="w-[100%]  lg:hidden">
+      <div className="w-[100%] block lg:hidden">
         <div
-          className="flex justify-end items-end w-[100%] transition-all duration-500 "
+          className="z-10 flex justify-end items-end  w-[100%] transition-all duration-500 "
           onClick={handleNav}
         >
           {!showNav ? (
-            <div className="flex justify-between items-center mx-[4rem] w-[100%] ">
+            <div className="flex justify-between items-center mx-[4rem] w-[100%]">
               <h1 className=" font-lora font-medium tracking-wider text-4xl hover:transition-all hover:duration-500 hover:text-[#4d574F]">
                 <Link href="/"> lg.</Link>
               </h1>
               <GrMenu className="text-6xl hover:bg-[#d9d4d0] hover:transition-all hover:duration-500 rounded p-2  " />
             </div>
           ) : (
-            <GrClose className="text-6xl z-20 rounded p-2 hover:bg-[#d9d4d0] hover:transition-all hover:duration-500 mx-[4rem]" />
+            <GrClose className="text-6xl z-50 rounded p-2 hover:bg-[#d9d4d0] hover:transition-all hover:duration-500 mx-[4rem]" />
           )}
         </div>
 
         <div
           className={
             !showNav
-              ? "flex opacity-0 h-[100%] z-0 transition-all duration-1000"
-              : "z-10 flex justify-center items-center h-full opacity-100 w-[100%] transition-all duration-1000"
+              ? "hidden"
+              : "absolute  z-40 top-0 left-0 w-[100%] transition-all duration-1000"
           }
         >
-          <ul className="flex flex-col justify-center items-center mt-[5rem] font-openSans font-thin text-2xl w-[100%] h-full">
+          <ul className="z-30 flex flex-col  bg-white justify-center items-center pt-[5rem] font-openSans font-thin text-2xl w-[100%] h-full">
             <Link href="/">
               <li className="uppercase tracking-wider m-[2rem] ">Home</li>
             </Link>
