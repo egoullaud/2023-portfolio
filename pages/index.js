56c65369b560018aee5b279eb6370b853dec85a1
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-10 18:26:04 -0700

    contact added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Work from "@/components/Work";
 import Hero from "../components/Hero";
+import ContactHero from "@/components/ContactHero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Work />
+      <ContactHero />
     </main>
   );
 }
