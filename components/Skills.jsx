e0b62fe6a49c11dbe719337f4b9b3920cf5112fe
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-11 11:29:13 -0700

    added content

---
+++ components/Skills.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <div className="my-[6rem] mx-[2rem]">
+      <h3 className="font-lora text-left leading-[1.2] lowercase tracking-widest mb-[2rem] text-3xl md:text-[2rem]">
+        my skills
+      </h3>
+      <ul className="columns-2 font-openSans font-thin">
+        <li className="mb-4">JavaScript</li>
+        <li className="mb-4">React</li>
+        <li className="mb-4">TailwindCSS</li>
+
+        <li className="mb-4">RESTful APIs</li>
+        <li className="mb-4">CSS3</li>
+        <li className="mb-4">HTML5</li>
+        <li className="mb-4">Bootstrap</li>
+        <li className="mb-4">Web Accessiblity</li>
+        <li className="mb-4">Agile Methodology</li>
+        <br />
+        <li className="mb-4">NextJS</li>
+        <li className="mb-4">MongoDB</li>
+        <li className="mb-4">PostgreSQL</li>
+
+        <li className="mb-4">GraphQL</li>
+        <li className="mb-4">SQL</li>
+        <li className="mb-4"></li>
+        <li className="mb-4">Git</li>
+        <li className="mb-4">GitHub</li>
+        <li className="mb-4">Figma</li>
+        <li className="mb-4">Responsive Web Design</li>
+      </ul>
+    </div>
+  );
+}
