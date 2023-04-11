980de1feaf95341f0e4810688507330b51aa8154
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-11 12:36:07 -0700

    created

---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function contact() {
+  return (
+    <div>
+      <h1 className="text-left leading-[1.25] font-lora lowercase tracking-wider text-5xl md:text-6xl pb-[2rem]">
+        Let's Connect
+      </h1>
+    </div>
+  );
+}
+
+export default contact;
