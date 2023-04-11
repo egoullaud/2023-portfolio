4deb14785f72e5161d296852bd2f7f5904637501
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-11 13:24:46 -0700

    created

---
+++ pages/work.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function work() {
+  return (
+    <div>
+      <h1 className="text-left  leading-[1.25] font-lora lowercase tracking-wider text-5xl md:text-6xl pb-[2rem] ">
+        work
+      </h1>
+    </div>
+  );
+}
+
+export default work;
