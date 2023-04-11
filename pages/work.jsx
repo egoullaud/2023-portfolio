8889804b6c6e0e2978b469120813126c5de6d827
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-11 13:28:26 -0700

    formatted

--- pages/work.jsx
+++ pages/work.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function work() {
   return (
-    <div className="mx-[2rem] mt-[4rem] mb-[2rem]">
+    <div className="mx-[2rem] my-[4rem] ">
       <h1 className="text-left  leading-[1.25] font-lora lowercase tracking-wider text-5xl md:text-6xl pb-[2rem] ">
         work
       </h1>
