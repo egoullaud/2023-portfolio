23b088945aec9e2ee4035eae8641ca25946d2df1
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-11 12:31:16 -0700

    formatted lg

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,12 +9,12 @@ import {
 
 function Footer() {
   return (
-    <div className="bg-[#313d4a] pt-[4rem] pb-[1rem]">
-      <div className="mx-[4rem]">
-        <h1 className=" text-[#F4e9e7] font-lora font-medium tracking-wider text-4xl hover:transition-all hover:duration-500 hover:text-[#baccdb] ease-out duration-200">
+    <div className="bg-[#313d4a] pt-[4rem] lg:pt-[2rem] pb-[1rem]">
+      <div className="mx-[4rem] flex flex-col lg:flex-row lg:justify-between">
+        <h1 className="lg:my-[1rem] text-[#F4e9e7] font-lora font-medium tracking-wider text-4xl hover:transition-all hover:duration-500 hover:text-[#baccdb] ease-out duration-200">
           <Link href="/"> lg.</Link>
         </h1>
-        <ul className="z-30 flex flex-col text-[#F4e9e7] justify-center items-start pt-[1rem] font-openSans font-thin  w-[100%] h-full">
+        <ul className="z-30 flex flex-col lg:flex-row lg:justify-evenly lg:w-[40%] lg:pt-0 lg:my-[1rem] text-[#F4e9e7] justify-center items-start pt-[1rem] font-openSans font-thin  w-[100%] h-full">
           <Link href="/">
             <li className="uppercase tracking-wider my-[.5rem]  hover:transition-all hover:duration-500  hover:text-[#baccdb] ease-out duration-200">
               Home
@@ -35,7 +35,9 @@ function Footer() {
               Contact
             </li>
           </Link>
-          <li className=" flex justify-between uppercase tracking-wider mt-[1rem] ">
+        </ul>
+        <div className="text-[#F4e9e7]">
+          <p className=" flex justify-start uppercase tracking-wider mt-[1rem] lg:my-[1rem] ">
             <Link
               href="https://www.linkedin.com/in/lee-goullaud/"
               target="_blank"
@@ -45,10 +47,10 @@ function Footer() {
             <Link href="https://github.com/egoullaud" target="_blank">
               <RiGithubFill className=" ml-2 text-2xl hover:text-[#baccdb] ease-out duration-200hover:transition-all hover:duration-500 " />
             </Link>
-          </li>
-        </ul>
+          </p>
+        </div>
       </div>
-      <p className="mt-[1rem] text-center uppercase font-openSans font-thin text-sm text-[#F4e9e7]">
+      <p className="mt-[1rem] lg:my-[1rem] text-center uppercase font-openSans font-thin text-sm text-[#F4e9e7]">
         © 2023 lee goullaud
       </p>
     </div>
