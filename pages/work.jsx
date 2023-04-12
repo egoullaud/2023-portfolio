786276c27b7eff8e4f66ec563dd5e40b09943199
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-12 15:26:21 -0700

    graphql calls

--- pages/work.jsx
+++ pages/work.jsx
@@ -1,13 +1,70 @@
 import React from "react";
+import client from "../apolloClient";
+import { gql } from "@apollo/client";
+import { ProjectCoverList } from "@/components/ProjectCoverList";
 
-function work() {
+function work({ projects }) {
   return (
-    <div className="mx-[2rem] my-[4rem] ">
-      <h1 className="text-left  leading-[1.25] font-lora lowercase tracking-wider text-5xl md:text-6xl pb-[2rem] ">
+    <div className=" flex flex-col items-center mx-[2rem] my-[4rem] ">
+      <h1 className="text-left leading-[1.25] font-alike lowercase tracking-wider text-5xl md:text-6xl pb-[2rem] md:pb-[4rem]">
         work
       </h1>
+      <ProjectCoverList projects={projects} />
     </div>
   );
 }
 
 export default work;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      {
+        projects {
+          title
+          slug
+          id
+          websiteUrl
+          githubUrl
+          preview
+          coverImage {
+            url
+            height
+            width
+          }
+          deskImage {
+            url
+            height
+            width
+          }
+
+          mobileImage {
+            url
+            height
+            width
+          }
+          tabletImage {
+            url
+            height
+            width
+          }
+          description {
+            html
+          }
+          testimonials {
+            author
+            id
+            description {
+              html
+            }
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      projects: data.projects,
+    },
+  };
+}
