f366e2f63de6dcc6c040a5767afd2c9c9b11c3f7
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-12 15:25:28 -0700

    typo

--- components/ContactHero.jsx
+++ components/ContactHero.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 function ContactHero() {
   return (
     <div className="bg-[#d9d4d0] py-[7rem] flex flex-col items-center">
-      <h3 className="font-alike text-center leading-[1.2] lowercase tracking-widest pb-[1rem] md:pb-[2rem] text-[1.2rem] md:text-[2rem] lg:text-2xl xl:text-4xl">
-        lets work together!
+      <h3 className="font-alike text-center leading-[1.2] lowercase tracking-widest mb-[2rem] text-xl md:text-[2rem]">
+        let's work together!
       </h3>
       <Link href="/contact">
         <button className="text-[1rem] font-hind uppercase tracking-wider font-thin hover:transition-all hover:duration-500 hover:text-[#69808f] ease-out duration-500 md:text-[1.2rem]">
