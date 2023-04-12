040dab6a8a04ae6a6d66e79d3bfc1ccf4594b29d
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-12 13:28:59 -0700

    style updated

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import portrait1 from "../public/portrait1.jpg";
+import aboutMe from "../public/about-me.png";
 import landscape1 from "../public/landscape1.jpg";
 import me2 from "../public/IMG-2424.jpg";
 import doggos from "../public/doggos.jpg";
@@ -12,46 +12,48 @@ import Skills from "@/components/Skills";
 
 function about() {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col lg:flex-row">
-        <div className="w-[100%] md:w-[55%] lg:w-[50%] xl:w-[50%]">
-          <Image src={me2} className="object-cover"></Image>
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex ">
+        <div className="w-[100%] md:w-[75%] lg:w-[45%] xl:w-[45%] lg:ml-[8rem] xl:ml-[10rem]">
+          <Image src={aboutMe} className="object-cover"></Image>
         </div>
-
-        <div className="flex flex-col mx-[2rem] lg:mx-[2rem] lg:w-[55%] lg:mt-[1rem] md:ml-[12rem] md:mt-[4rem] md:mr-[10rem] mt-[6rem] ">
-          <h1 className="text-left leading-[1.25] font-lora lowercase tracking-wider text-5xl md:text-6xl pb-[2rem]">
-            Hey, I'm lee!
+        <div className=" relative justify-center left-[-10%] md:left-[-8%] lg:left-[-5%] xl:left-[-0%] w-[63%] md:w-[50%] lg:w-[55%] xl:w-[55%] mt-[4rem]  md:mt-[9rem] lg:mt-[8rem] xl:mt-[10rem]">
+          <h1 className="w-[100%] text-left font-alike  tracking-wide text-[1.7rem] md:text-6xl xl:text-7xl">
+            Hi, I'm Lee!
           </h1>
-          <p className="font-openSans font-thin md:ml-[2rem] mb-2 lg:mr-[6rem]">
-            I am a fullstack developer who's passionate about building digital
-            products that create accessibile, inclusive, and beautiful user
-            experiences. I am a graduate of Skillcrush UX Design and Front End
-            Coding Bootcamp and hold a BSc in Natural Resource Conservation from
-            the University of British Columbia.
-          </p>
-          <p className="font-openSans font-thin md:ml-[2rem] mb-2 lg:mr-[6rem]">
-            I have a deep love for learning and growth in all things, and I am
-            at my best when I am creating value through custom-built websites
-            for my clients. If you're interested in working together,{" "}
-            <Link href="/contact">
-              <button className="underline hover:transition-all hover:duration-500 hover:text-[#4d574F] ease-out duration-300">
-                get in touch here!
-              </button>
-            </Link>
-          </p>
-          <p className="font-openSans font-thin md:ml-[2rem] mb-2 lg:mr-[6rem]">
-            If you are a company or recruiter looking to hire a fullstack
-            developer,{" "}
-            <Link href="/">
-              <button className="underline hover:transition-all hover:duration-500 hover:text-[#4d574F] ease-out duration-300">
-                check out my resume here!
-              </button>
-            </Link>
-          </p>
         </div>
       </div>
+      <div className="flex flex-col font-hind tracking-[.03em] font-thin text-center mx-[2rem] md:mx-[4rem] lg:mx-[8rem] xl:w-[55%] lg:w-[65%] mt-[2rem] md:mt-[3rem] lg:mt-[4rem]  ">
+        <p className="mb-4">
+          I am a fullstack developer who's passionate about building digital
+          products that create accessibile, inclusive, and beautiful user
+          experiences. I am a graduate of Skillcrush UX Design and Front End
+          Coding Bootcamp and hold a BSc in Natural Resource Conservation from
+          the University of British Columbia.
+        </p>
+        <p className="mb-4">
+          I have a deep love for learning and growth in all things, and I am at
+          my best when I am creating value through custom-built websites for my
+          clients and playing fetch with my dogs. If you're interested in
+          working together,{" "}
+          <Link href="/contact">
+            <button className="underline hover:transition-all hover:duration-500 hover:text-[#9aa9b5] ease-out duration-500">
+              get in touch here!
+            </button>
+          </Link>
+        </p>
+        <p className="font-openSans font-thin md:ml-[2rem] mb-2 lg:mr-[6rem]">
+          If you are a company or recruiter looking to hire a fullstack
+          developer,{" "}
+          <Link href="/">
+            <button className="underline hover:transition-all hover:duration-500 hover:text-[#9aa9b5] ease-out duration-500">
+              check out my resume here!
+            </button>
+          </Link>
+        </p>
+      </div>
       <Skills />
-      <div className="flex flex-col items-center justify-center md:flex-row px-[2rem] pb-[2rem]">
+      <div className="flex flex-col items-center justify-center md:flex-row px-[3rem] pb-[2rem] md:px-[4rem] lg:px-[8rem] xl:px-[14rem]">
         <Image
           className="mb-6 md:w-[27%] object-fit md:mr-4"
           src={portrait2}
