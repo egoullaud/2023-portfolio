de92dda18ab3d22bc75a8ffff9f90585c78834bd
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-12 15:26:36 -0700

    added queries

--- services/index.jsx
+++ services/index.jsx
@@ -1,3 +1,4 @@
+import { gql } from "@apollo/client";
 export const PROJECTS_QUERY = gql`
   {
     projects {
@@ -9,16 +10,24 @@ export const PROJECTS_QUERY = gql`
       preview
       coverImage {
         url
+        height
+        width
       }
       deskImage {
         url
+        height
+        width
       }
 
       mobileImage {
         url
+        height
+        width
       }
       tabletImage {
         url
+        height
+        width
       }
       description {
         html
@@ -41,6 +50,22 @@ export const TESTIMONIALS_QUERY = gql`
       id
       description {
         html
+      }
+    }
+  }
+`;
+
+export const PROJECT_COVER_QUERY = gql`
+  {
+    projects(first: 4) {
+      title
+      slug
+      id
+      preview
+      coverImage {
+        url
+        height
+        width
       }
     }
   }
