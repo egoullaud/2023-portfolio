f0df73e06e65cb2126f98d3c59313c94d4d1e568
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-12 15:26:14 -0700

    graphql calls

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,38 @@
 import Work from "@/components/Work";
 import Hero from "../components/Hero";
 import ContactHero from "@/components/ContactHero";
+import client from "../apolloClient";
+import { gql } from "@apollo/client";
 
-export default function Home() {
+export default function Home({ projects }) {
   return (
     <main>
       <Hero />
-      <Work />
+      <Work projects={projects} />
       <ContactHero />
     </main>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      {
+        projects(first: 4) {
+          title
+          slug
+          id
+          preview
+          coverImage {
+            url
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      projects: data.projects,
+    },
+  };
 }
