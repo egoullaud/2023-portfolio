1421d51f17e849ccf00e04404b62c9307dfaa629
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-13 09:22:02 -0700

    fixed inner text format

--- components/ProjectCoverList.jsx
+++ components/ProjectCoverList.jsx
@@ -9,12 +9,12 @@ export function ProjectCoverList({ projects }) {
         <Link key={project.id} href={"/projects/" + project.slug}>
           <li className="mx-8 my-8 md:mx-0 md:mb-4 md:mt-0 shadow-md hover:scale-95 hover:transition-all hover:duration-500 ease-out duration-500">
             <div className="relative">
-              <div className="absolute inset-0 z-10 px-5 flex flex-col items-center justify-center bg-[#9aa9b5] opacity-0 hover:opacity-100 ease-out duration-500 hover:transition-all hover:duration-500 hover:text-white">
-                <div className="mx-10 w-[70%] tracking-wider">
+              <div className="absolute inset-0 z-10 flex flex-col items-center justify-center bg-[#7f8c96] opacity-0 hover:opacity-100 ease-out duration-500 hover:transition-all hover:duration-500 hover:text-white">
+                <div className="mx-0 w-[90%] tracking-wider">
                   <h6 className="font-alike text-center text-2xl mb-4">
                     {project.title}
                   </h6>
-                  <p className="font-hind mx-4 text-center">
+                  <p className="font-hind mx-2 text-center">
                     {project.preview}
                   </p>
                 </div>
